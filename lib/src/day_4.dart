8b3eed5c43b9c994f41cafdb0ed82daaea34b9ba
RafaAudibert <rafaeelaudibert@gmail.com>
2019-12-04 03:06:05 -0300

    :recycle: Improve day 4 code

--- lib/src/day_4.dart
+++ lib/src/day_4.dart
@@ -11,57 +11,39 @@ List<int> _processInput() => readFromFiles(day: 4, part: 1)
     .map((i) => int.parse(i))
     .toList();
 
-day_4_part_1() {
-  var data = _processInput();
-  var r1 = data[0];
-  var r2 = data[1];
-
-  var counter = 0;
+List<String> validOnes(int r1, int r2) {
+  List<String> valids = List();
   for (var i = r1; i <= r2; i++) {
     var text = i.toString();
 
     var can = true;
     var two_adjacent = false;
+
     for (var l = 1; l < text.length; l++) {
       if (text[l] == text[l - 1]) two_adjacent = true;
       if (int.parse(text[l]) < int.parse(text[l - 1])) can = false;
     }
-    if (can && two_adjacent) counter++;
+
+    if (can && two_adjacent) valids.add(text);
   }
 
-  return counter;
+  return valids;
 }
 
-day_4_part_2() {
-  var data = _processInput();
-  var r1 = data[0];
-  var r2 = data[1];
+day_4_part_1() => validOnes(_processInput()[0], _processInput()[1]).length;
 
-  var counter = 0;
-  for (var i = r1; i <= r2; i++) {
-    var text = i.toString();
-
-    var can = true;
-    var two_adjacent = false;
-    Set<int> seqs = Set();
-    int curr_seq = 1;
-    for (var l = 1; l < text.length; l++) {
-      if (int.parse(text[l]) < int.parse(text[l - 1]))
-        can = false; // Decreasing
-
-      if (text[l] == text[l - 1]) {
-        // Equal to the last
-        two_adjacent = true;
-        curr_seq++;
-      } else {
-        seqs.add(curr_seq);
-        curr_seq = 1;
+day_4_part_2() =>
+    validOnes(_processInput()[0], _processInput()[1]).where((text) {
+      int curr_seq = 1;
+      for (var l = 1; l < text.length; l++) {
+        if (text[l] == text[l - 1]) {
+          curr_seq++;
+        } else if (curr_seq == 2) {
+          return true;
+        } else {
+          curr_seq = 1;
+        }
       }
-    }
-    seqs.add(curr_seq); // Add the last one
 
-    if (can && two_adjacent && seqs.contains(2)) counter++;
-  }
-
-  return counter;
-}
+      return curr_seq == 2; // Last one check
+    }).length;
