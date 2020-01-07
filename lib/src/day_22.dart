a4f7c46adea7fe59693c38eb364fd9d6084e9542
RafaAudibert <rafaeelaudibert@gmail.com>
2020-01-07 03:47:24 -0300

    :sparkles: Added day 22 part 2

--- lib/src/day_22.dart
+++ lib/src/day_22.dart
@@ -54,5 +54,58 @@ day_22_part_1() {
   return stack.indexOf(2019);
 }
 
-// Part 2 requires modular inverse which I'm not aware of the theory
-day_22_part_2() => throw UnimplementedError();
+// Based on [/u/metalim](https://www.reddit.com/user/metalim/) Python code, available
+// [here](https://github.com/metalim/metalim.adventofcode.2019.python/blob/master/22_cards_shuffle.ipynb)
+day_22_part_2() {
+  var data = _processInput();
+
+  var L = BigInt.from(119315717514047);
+  var N = BigInt.from(101741582076661);
+  return shuffle2(L, N, 2020, data);
+}
+
+// convert rules to linear polynomial.
+// (g∘f)(x) = g(f(x))
+Tuple2<BigInt, BigInt> parse(BigInt L, List<Tuple2<Shuffle, int>> shuffles) {
+  var a = BigInt.one, b = BigInt.zero;
+  for (var shuffle in shuffles.reversed) {
+    var shuffle_type = shuffle.item1;
+    var quantity = BigInt.from(shuffle.item2);
+
+    switch (shuffle_type) {
+      case Shuffle.DealIncrement:
+        quantity = quantity.modInverse(L);
+        a = a * quantity % L;
+        b = b * quantity % L;
+        break;
+      case Shuffle.Cut:
+        b = (b + quantity) % L;
+        break;
+      case Shuffle.DealStack:
+        a = -a;
+        b = L - b - BigInt.one;
+        break;
+    }
+  }
+
+  return Tuple2(a, b);
+}
+
+// modpow the polynomial: (ax+b)^m % n
+// f(x) = ax+b
+// g(x) = cx+d
+// f^2(x) = a(ax+b)+b = aax + ab+b
+// f(g(x)) = a(cx+d)+b = acx + ad+b
+Tuple2<BigInt, BigInt> polypow(BigInt a, BigInt b, BigInt m, BigInt n) {
+  if (m == BigInt.zero) return Tuple2(BigInt.one, BigInt.zero);
+  if (m.isEven) return polypow(a * a % n, (a * b + b) % n, m ~/ BigInt.two, n);
+
+  var tuple = polypow(a, b, m - BigInt.one, n);
+  return Tuple2(a * tuple.item1 % n, (a * tuple.item2 + b) % n);
+}
+
+int shuffle2(BigInt L, BigInt N, int pos, List<Tuple2<Shuffle, int>> rules) {
+  var parsed = parse(L, rules);
+  parsed = polypow(parsed.item1, parsed.item2, N, L);
+  return (pos * parsed.item1.toInt() + parsed.item2.toInt()) % L.toInt();
+}
