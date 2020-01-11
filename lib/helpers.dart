20ffacb3297ba46c58810cb0bc6ed48d71e27e50
RafaAudibert <rafaeelaudibert@gmail.com>
2020-01-11 05:04:43 -0300

    :memo: Updated helpers docs

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -11,26 +11,25 @@ import 'dart:io';
 String readFromFiles({day, part}) =>
     File('./lib/data/day_${day}_${part}.txt').readAsStringSync();
 
-// Return a range between start and end
+/// Return a range between [start] and [end]
 List<int> range(int start, int end) =>
     new List<int>.generate(end - start, (i) => i + start);
 
-// Create a sequence, given an initial number and a seed,
-// until a condition is reached
-Iterable<T> sequence<T>(T seed, T next(T current) bool condition(T current)) sync* {
-  for (var current = seed; condition(current); current = next(current)) yield current;
+/// Create a sequence, given an initial [seed],
+/// until a [condition] is reached
+Iterable<T> sequence<T>(
+    T seed, T next(T current), bool condition(T current)) sync* {
+  for (var current = seed; condition(current); current = next(current))
+    yield current;
 }
 
-
-// Generate all the possible tuples from a list
+/// Generate all the possible tuples from a [list]
 Iterable<List<T>> generate_tuples<T>(List<T> list) sync* {
   for (var i in range(0, list.length))
-    for (var j in range(0, list.length))
-      if (i != j)
-        yield [list[i], list[j]];
+    for (var j in range(0, list.length)) if (i != j) yield [list[i], list[j]];
 }
 
-// Point class
+/// [Point] class
 class Point {
   int x, y;
   Point(this.x, this.y);
