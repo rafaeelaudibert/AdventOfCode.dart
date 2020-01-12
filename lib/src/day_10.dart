151babf143d8ecef531e18e8d0e2528e82dc0b4d
RafaAudibert <rafaeelaudibert@gmail.com>
2020-01-11 23:49:28 -0300

    :sparkles::claus: Added day 10 part 2

--- lib/src/day_10.dart
+++ lib/src/day_10.dart
@@ -10,41 +10,79 @@ List<String> _processInput() =>
     readFromFiles(day: 10, part: 1).split('\n').toList();
 
 // Global size
-const SIZE = 20;
+const SIZE = 21;
 
-List<List<int>> getAsteroids() {
+// Parse input and get all asteroids
+List<Point> getAsteroids() {
   var data = _processInput();
 
-  List<List<int>> asteroids = [
+  return [
     for (var i in range(0, SIZE))
-      for (var j in range(0, SIZE)) if (data[i][j] == '#') [i, j]
+      for (var j in range(0, SIZE)) if (data[i][j] == '#') Point(i, j)
   ];
-
-  return asteroids;
 }
 
 // Hard to believe it works with floating point
-List<double> can_see(int x, int y, List<List<int>> asteroids) =>
-    Set<double>.from([
-      for (var asteroid in asteroids)
-        if (asteroid[0] != x || asteroid[1] != y)
-          atan2(y - asteroid[1], x - asteroid[0])
-    ]).toList();
+Set<double> can_see(Point asteroid, List<Point> asteroids) => Set<double>.from([
+      for (var other_asteroid in asteroids)
+        if (other_asteroid.x != asteroid.x || other_asteroid.y != asteroid.y)
+          atan2(asteroid.y - other_asteroid.y, asteroid.x - other_asteroid.x)
+    ]);
 
-List<Map<String, List<double>>> get_how_many() {
+Iterable<Set<double>> get_how_many() {
   var asteroids = getAsteroids();
 
-  return asteroids
-      .map((List<int> asteroid) => ({
-            'angles': can_see(asteroid[0], asteroid[1], asteroids),
-            'x': [asteroid[0] * 1.0],
-            'y': [asteroid[1] * 1.0]
-          }))
-      .toList();
+  return asteroids.map((Point asteroid) => can_see(asteroid, asteroids));
 }
 
-day_10_part_1() =>
-    get_how_many().map((asteroid) => asteroid['angles'].length).reduce(max);
+day_10_part_1() => get_how_many()
+    .fold(0, (int maximum, var asteroid) => max(maximum, asteroid.length));
 
-// Can't figure that out
-day_10_part_2() => throw UnimplementedError();
+day_10_part_1_different() =>
+    get_how_many().map<int>((asteroid) => asteroid.length).reduce(max);
+
+// Based on [julemand101](https://github.com/julemand101/) code available
+// [here](https://github.com/julemand101/AdventOfCode2019/blob/master/lib/day10.dart)
+day_10_part_2() {
+  final asteroids = getAsteroids();
+
+  final station = findStation(asteroids);
+  asteroids.remove(station);
+
+  var asteroidsDestroyed = 0;
+  while (asteroids.isNotEmpty) {
+    final angleToAsteroid = <double, Point>{};
+
+    for (final asteroid in asteroids) {
+      final angle = atan2(asteroid.y - station.y, asteroid.x - station.x);
+
+      if (angleToAsteroid.containsKey(angle)) {
+        if (asteroid.dist(station) < angleToAsteroid[angle].dist(station)) {
+          angleToAsteroid[angle] = asteroid;
+        }
+      } else {
+        angleToAsteroid[angle] = asteroid;
+      }
+    }
+
+    final anglesOfAsteroidsToDestroy = angleToAsteroid.keys.toList()..sort();
+    for (final degree in anglesOfAsteroidsToDestroy.reversed) {
+      final asteroidToRemove = angleToAsteroid[degree];
+      asteroids.remove(asteroidToRemove);
+
+      if (++asteroidsDestroyed == 200) {
+        return (asteroidToRemove.y * 100) + asteroidToRemove.x;
+      }
+    }
+  }
+
+  throw FallThroughError();
+}
+
+Point findStation(List<Point> asteroids) {
+  var maximum_how_many = day_10_part_1();
+
+  return asteroids.elementAt(get_how_many()
+      .toList()
+      .indexWhere((asteroids) => asteroids.length == maximum_how_many));
+}
